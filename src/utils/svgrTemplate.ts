9a67f9fa9b1a52dede25e04d0201ce1c3560e5a1
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-11-02 21:52:01 -0300

    fix: fixing problem ref not working

--- src/utils/svgrTemplate.ts
+++ src/utils/svgrTemplate.ts
@@ -3,11 +3,10 @@ export const template = (variables, { tpl }) => {
     ${variables.imports};
 
     interface componentProps extends SVGProps<SVGSVGElement> {
-      ref?: Ref<SVGSVGElement>;
       fr?: any;
     }
 
-    const ${variables.componentName} = ({ref,fr=undefined, ...props}: componentProps) => (
+    const ${variables.componentName} = (props: componentProps, ref: Ref<SVGSVGElement>) => (
       ${variables.jsx}
     );
     
